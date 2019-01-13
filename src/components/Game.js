18d3bd63769cb6837ac176e4974ff79c9a606670
sharjeelhaider <sharjeelhaider@digitify.com>
2019-01-13 19:56:45 +0500

    Initial commit

---
+++ src/components/Game.js
@@ -0,0 +1,160 @@
+import React, {Component} from 'react';
+import {View, Text, Button, StyleSheet} from 'react-native';
+import PropTypes from 'prop-types';
+import RandomNumber from './RandomNumber';
+import shuffle from 'lodash.shuffle';
+
+class Game extends Component {
+
+    static propTypes = {
+        randomNumberCount: PropTypes.number.isRequired,
+        initialSecond: PropTypes.number.isRequired,
+        onPlayAgain: PropTypes.func.isRequired,
+    };
+
+    state = {
+        selectedIds: [],
+        remainingSeconds: this.props.initialSecond,
+    };
+
+    gameStatus = 'PLAYING';
+
+    randomNumbers = Array
+        .from({length: this.props.randomNumberCount})
+        .map(() => 1 + Math.floor(10 * Math.random()));
+    target = this.randomNumbers
+        .slice(0, this.props.randomNumberCount - 2)
+        .reduce((acc, curr) => acc + curr, 0);
+    shuffledRandomNumbers = shuffle(this.randomNumbers);
+
+    componentWillUpdate(nextProps, nextState) {
+        if (nextState.selectedIds !== this.state.selectedIds || nextState.remainingSeconds === 0) {
+            this.gameStatus = this.calcGameStatus(nextState);
+            if (this.gameStatus !== 'PLAYING') {
+                clearInterval(this.intervalId)
+            }
+        }
+    }
+
+    componentDidMount(): void {
+        this.intervalId = setInterval(() => {
+            this.setState((prevState) => {
+                return {remainingSeconds: prevState.remainingSeconds - 1};
+            }, () => {
+                if (this.state.remainingSeconds === 0) {
+                    clearInterval(this.intervalId)
+                }
+            });
+        }, 1000);
+    }
+
+    componentWillUnmount(): void {
+        clearInterval(this.intervalId);
+    }
+
+    isNumberSelected = (numberIndex) => {
+        return this.state.selectedIds.indexOf(numberIndex) >= 0;
+    }
+
+    selectNumber = (numberIndex) => {
+        this.setState((prevState) => ({
+            selectedIds: [...prevState.selectedIds, numberIndex],
+        }));
+    };
+
+    // calcGameStatus: PLAYING, WON, LOST
+    calcGameStatus = (nextState) => {
+        const sumSelected = nextState.selectedIds.reduce((acc, curr) => {
+            return acc + this.shuffledRandomNumbers[curr];
+        }, 0);
+
+        if (nextState.remainingSeconds === 0) {
+            return 'LOST'
+        }
+        if (sumSelected < this.target) {
+            return 'PLAYING'
+        }
+        if (sumSelected == this.target) {
+            return 'WON'
+        }
+        if (sumSelected > this.target) {
+            return 'LOST'
+        }
+        console.log(sumSelected);
+    }
+
+    render() {
+        const gameStatus = this.gameStatus;
+        return (
+            <View style={styles.container}>
+                <Text style={[styles.target, styles[`STATUS_${gameStatus}`]]}> {this.target}</Text>
+                <View style={styles.randomContainer}>
+                    {
+                        this.shuffledRandomNumbers.map((randomNumber, index) =>
+                            <RandomNumber
+                                key={index}
+                                id={index}
+                                number={randomNumber}
+                                isDisabled={
+                                    this.isNumberSelected(index) || gameStatus !== 'PLAYING'
+                                }
+                                onPress={this.selectNumber}
+                            />
+                        )
+                    }
+
+                </View>
+
+                {this.gameStatus !== 'PLAYING' && (
+                    <Button title="Play Again" onPress={() => {
+                        this.props.onPlayAgain()
+                    }}/>
+                )}
+                <Text>{this.state.remainingSeconds}</Text>
+
+            </View>
+        );
+    }
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#ddd',
+        flex: 1,
+    },
+    target: {
+        fontSize: 40,
+        backgroundColor: '#bbb',
+        margin: 50,
+        textAlign: 'center',
+
+    },
+
+    randomContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around'
+    },
+
+    random: {
+        backgroundColor: '#999',
+        width: 100,
+        marginHorizontal: 15,
+        marginVertical: 25,
+        fontSize: 35,
+        textAlign: 'center'
+    },
+
+    STATUS_PLAYING: {
+        backgroundColor: '#bbb'
+    },
+    STATUS_WON: {
+        backgroundColor: 'green'
+    },
+    STATUS_LOST: {
+        backgroundColor: 'red'
+    },
+});
+export default Game;
